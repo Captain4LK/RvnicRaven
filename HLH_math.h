28e3c95a82bf6cb0cb6ade84fada369442b4622d
Captain4LK <captain4lk@tutanota.com>
2021-08-28 16:03:48 +0200

    Continue HLH_math

--- HLH_math.h
+++ HLH_math.h
@@ -22,6 +22,16 @@ typedef struct { float x,y; }HLH_vec2;
 typedef struct { float x,y,z; }HLH_vec3;
 typedef struct { float x,y,z,w; }HLH_vec4;
 
+//Utility
+float HLH_to_radians(float degrees);
+float HLH_to_degrees(float radians);
+float HLH_lerp(float a, float b, float t);
+float HLH_sign(float f);
+float HLH_min(float a, float b);
+float HLH_max(float a, float b);
+float HLH_clamp(float lo, float hi, float f);
+float HLH_abs(float f);
+
 //2d vector math
 HLH_vec2 HLH_vector2(float x, float y);
 HLH_vec2 HLH_add2(HLH_vec2 a, HLH_vec2 b);
@@ -36,6 +46,7 @@ float    HLH_dot2(HLH_vec2 a, HLH_vec2 b);
 float    HLH_mag2(HLH_vec2 a);
 float    HLH_magmag2(HLH_vec2 a);
 HLH_vec2 HLH_norm2(HLH_vec2 a);
+HLH_vec2 HLH_lerp2(HLH_vec2 a, HLH_vec2 b, float t);
 
 //3d vector math
 HLH_vec3 HLH_vector3(float x, float y, float z);
@@ -52,6 +63,7 @@ float    HLH_mag3(HLH_vec3 a);
 float    HLH_magmag3(HLH_vec3 a);
 HLH_vec3 HLH_norm3(HLH_vec3 a);
 HLH_vec3 HLH_cross3(HLH_vec3 a, HLH_vec3 b);
+HLH_vec3 HLH_lerp3(HLH_vec3 a, HLH_vec3 b, float t);
 
 //4d vector math
 HLH_vec4 HLH_vector4(float x, float y, float z, float w);
@@ -67,6 +79,7 @@ float    HLH_dot4(HLH_vec4 a, HLH_vec4 b);
 float    HLH_mag4(HLH_vec4 a);
 float    HLH_magmag4(HLH_vec4 a);
 HLH_vec4 HLH_norm4(HLH_vec4 a);
+HLH_vec4 HLH_lerp4(HLH_vec4 a, HLH_vec4 b, float t);
 
 #endif
 
@@ -75,6 +88,48 @@ HLH_vec4 HLH_norm4(HLH_vec4 a);
 #define HLH_MATH_IMPLEMENTATION_ONCE
 
 #include <math.h>
+
+float HLH_to_radians(float degrees)
+{
+   return degrees*(3.14159265359f/180.0f);
+}
+
+float HLH_to_degrees(float radians)
+{
+   return radians*(180.0f/3.14159265359f);
+}
+
+float HLH_lerp(float a, float b, float t)
+{
+   return (1.0f-t)*a+t*b;
+}
+
+float HLH_sign(float f)
+{
+   return f<0.0f?-1.0f:f>0.0f?1.0f:0.0f;
+}
+
+float HLH_min(float a, float b)
+{
+   return a<b?a:b;
+}
+
+float HLH_max(float a, float b)
+{
+   return a>b?a:b;
+}
+
+float HLH_clamp(float lo, float hi, float f)
+{
+   return HLH_min(hi,HLH_max(lo,f));
+}
+
+float HLH_abs(float f)
+{
+   if(f<0.0f)
+      return -f;
+   return f;
+}
 
 HLH_vec2 HLH_vector2(float x, float y)
 {
@@ -139,6 +194,12 @@ float HLH_magmag2(HLH_vec2 a)
 HLH_vec2 HLH_norm2(HLH_vec2 a)
 {
    return HLH_div2_f(a,HLH_mag2(a));
+}
+
+HLH_vec2 HLH_lerp2(HLH_vec2 a, HLH_vec2 b, float t)
+{
+   return HLH_vector2(HLH_lerp(a.x,b.x,t),
+                      HLH_lerp(a.y,b.y,t));
 }
 
 HLH_vec3 HLH_vector3(float x, float y, float z)
@@ -213,6 +274,13 @@ HLH_vec3 HLH_cross3(HLH_vec3 a, HLH_vec3 b)
                       a.x*b.y-a.y*b.x);
 }
 
+HLH_vec3 HLH_lerp3(HLH_vec3 a, HLH_vec3 b, float t)
+{
+   return HLH_vector3(HLH_lerp(a.x,b.x,t),
+                      HLH_lerp(a.y,b.y,t),
+                      HLH_lerp(a.z,b.z,t));
+}
+
 HLH_vec4 HLH_vector4(float x, float y, float z, float w)
 {
    return (HLH_vec4){.x = x, .y = y, .z = z, .w = w};
@@ -276,6 +344,14 @@ float HLH_magmag4(HLH_vec4 a)
 HLH_vec4 HLH_norm4(HLH_vec4 a)
 {
    return HLH_div4_f(a,HLH_mag4(a));
+}
+
+HLH_vec4 HLH_lerp4(HLH_vec4 a, HLH_vec4 b, float t)
+{
+   return HLH_vector4(HLH_lerp(a.x,b.x,t),
+                      HLH_lerp(a.y,b.y,t),
+                      HLH_lerp(a.z,b.z,t),
+                      HLH_lerp(a.w,b.w,t));
 }
 
 #endif
