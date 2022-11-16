5dd3586cf5a59fc077d7fe274ec82442dea211bb
Captain4LK <captain4lk@tutanota.com>
2022-11-16 19:38:36 +0100

    RvR_ppp: first version

--- headers/RvR_ppp.h
+++ headers/RvR_ppp.h
@@ -1,7 +1,7 @@
 #ifndef _RVR_PPP_H_
 
 /*
-   RvnicRaven - ppp streaming (de-)compressor
+   RvnicRaven - predictor streaming (de-)compressor
 
    Written in 2022 by Lukas Holzbeierlein (Captain4LK) email: captain4lk [at] tutanota [dot] com
 
@@ -27,11 +27,24 @@ typedef struct
    uint8_t byte;
    uint8_t byte_buf[8];
    uint8_t flag;
-}RvR_ppp_context;
+}RvR_ppp_ccontext;
 
-void RvR_ppp_compress_init(RvR_ppp_context *c, RvR_rw *dst);
-void RvR_ppp_compress_push(RvR_ppp_context *c, uint8_t byte);
-void RvR_ppp_compress_flush(RvR_ppp_context *c, uint8_t pad);
+typedef struct
+{
+   uint8_t guess_table[1<<18];
+   uint32_t hash;
+   RvR_rw *src;
+
+   uint8_t byte;
+   uint8_t flag;
+}RvR_ppp_dcontext;
+
+void RvR_ppp_compress_init(RvR_ppp_ccontext *c, RvR_rw *dst);
+void RvR_ppp_compress_push(RvR_ppp_ccontext *c, uint8_t byte);
+void RvR_ppp_compress_flush(RvR_ppp_ccontext *c, uint8_t pad);
+
+void RvR_ppp_decompress_init(RvR_ppp_dcontext *c, RvR_rw *src);
+int16_t RvR_ppp_decompress_pop(RvR_ppp_dcontext *c);
 
 #endif
 
@@ -39,17 +52,18 @@ void RvR_ppp_compress_flush(RvR_ppp_context *c, uint8_t pad);
 #ifndef RVR_PPP_IMPLEMENTATION_ONCE
 #define RVR_PPP_IMPLEMENTATION_ONCE
 
-void RvR_ppp_compress_init(RvR_ppp_context *c, RvR_rw *dst)
+void RvR_ppp_compress_init(RvR_ppp_ccontext *c, RvR_rw *dst)
 {
    memset(c->guess_table,0,sizeof(c->guess_table));
    c->hash = 0;
    c->dst = dst;
    c->flag = 0;
+   c->byte = 0;
 }
 
-void RvR_ppp_compress_push(RvR_ppp_context *c, uint8_t byte)
+void RvR_ppp_compress_push(RvR_ppp_ccontext *c, uint8_t byte)
 {
-   if(c->guess[c->hash]!=byte)
+   if(c->guess_table[c->hash]!=byte)
    {
       c->guess_table[c->hash] = byte;
       c->byte_buf[c->byte] = byte;
@@ -73,10 +87,42 @@ void RvR_ppp_compress_push(RvR_ppp_context *c, uint8_t byte)
    }
 }
 
-void RvR_ppp_compress_flush(RvR_ppp_context *c, uint8_t pad)
+void RvR_ppp_compress_flush(RvR_ppp_ccontext *c, uint8_t pad)
 {
    while(c->byte!=0)
       RvR_ppp_compress_push(c,pad);
+}
+
+void RvR_ppp_decompress_init(RvR_ppp_dcontext *c, RvR_rw *src)
+{
+   memset(c->guess_table,0,sizeof(c->guess_table));
+   c->hash = 0;
+   c->src = src;
+   c->flag = 0;
+   c->byte = 0;
+}
+
+int16_t RvR_ppp_decompress_pop(RvR_ppp_dcontext *c)
+{
+   if(c->byte==0)
+   {
+      if(RvR_rw_eof(c->src))
+         return -1;
+      c->flag = RvR_rw_read_u8(c->src);
+   }
+
+   uint8_t byte;
+   if(c->flag&(1<<c->byte))
+      byte = c->guess_table[c->hash];
+   else
+      c->guess_table[c->hash] = byte = RvR_rw_read_u8(c->src);
+
+   c->hash = ((c->hash*160)^(byte))&((1<<18)-1);
+   c->byte++;
+   if(c->byte==8)
+      c->byte = 0;
+
+   return byte;
 }
 
 #endif
