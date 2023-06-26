195208c254f8f943cef3428417d5d9368d112565
Captain4LK <captain4lk@tutanota.com>
2023-06-26 19:37:21 +0200

    iso: rendering progress

---
+++ examples/iso/src/linux.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcc -o ../bin/iso *.c ../../../RvR_ray/libRvR_ray.a ../../../RvR/libRvR.a -std=c99 -I ../../../include/ -Wall -Wextra -Wshadow -pedantic -lSDL2 -lSDL2_mixer -O3 -g -lm  -lprofiler
