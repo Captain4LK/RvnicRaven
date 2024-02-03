0bd23361324ba2b9b106d908e6894bdd27b834f3
Captain4LK <captain4lk@tutanota.com>
2024-02-03 20:18:48 +0100

    RvR_portal: more checks in map_check

--- RvR_portal/RvR_port_map.c
+++ RvR_portal/RvR_port_map.c
@@ -224,6 +224,18 @@ int RvR_port_map_check(const RvR_port_map *map)
       }
    }
 
+   //Check sectors
+   for(int i = 0;i<map->sector_count;i++)
+   {
+      RvR_error_check(map->sectors[i].wall_first>=0&&map->sectors[i].wall_first<map->wall_count,"RvR_port_map_check","wall_first %d for sector %d out of bounds\n",map->sectors[i].wall_first,i);
+      RvR_error_check(map->sectors[i].wall_count>0,"RvR_port_map_check","wall_count %d for sector %d invalid\n",map->sectors[i].wall_count,i);
+      RvR_error_check(map->sectors[i].wall_first+map->sectors[i].wall_count<=map->wall_count,"RvR_port_map_check","wall range (%d, %d) of sector %d out of bounds\n",map->sectors[i].wall_first,map->sectors[i].wall_first+map->sectors[i].wall_count-1,i);
+   }
+
+   //TODO(Captain4LK): check p2 of walls:
+   //must be in sector
+   //must be either wall+1, or first wall of subsector
+
    //Check portals
    for(int i = 0;i<map->wall_count;i++)
    {
@@ -254,6 +266,63 @@ int RvR_port_map_check(const RvR_port_map *map)
       RvR_fix22 y11 = map->walls[map->walls[pwall].p2].y;
 
       RvR_error_check(x00==x11&&y00==y11&& x01==x10&&y01==y10, "RvR_port_map_check","wall %d and portal_wall %d don't match up\n",i,pwall);
+   }
+
+   //Check winding
+   for(int i = 0;i<map->sector_count;i++)
+   {
+      int wall = 0;
+      int subsector = 0;
+      RvR_port_sector *s = map->sectors+i;
+      while(wall<map->sectors[i].wall_count-1)
+      {
+         int64_t sum = 0;
+         int first = wall;
+
+         //Calculate winding number, positive if clockwise
+         for(;wall==first||map->walls[map->sectors[i].wall_first+wall-1].p2==map->sectors[i].wall_first+wall;wall++)
+         {
+            int64_t x0 = map->walls[s->wall_first+wall].x;
+            int64_t y0 = map->walls[s->wall_first+wall].y;
+            int64_t x1 = map->walls[map->walls[s->wall_first+wall].p2].x;
+            int64_t y1 = map->walls[map->walls[s->wall_first+wall].p2].y;
+            sum+=(x1-x0)*(y0+y1);
+         }
+
+         //First subsector must be clockwise, all other subsectors counter-clockwise
+         RvR_error_check(! ((first==0&&sum>0)||(first!=0&&sum<0)),"RvR_port_map_check","winding of subsector %d of sector %d incorrect\n",subsector,i);
+
+         subsector++;
+      }
+   }
+
+   //Check for overlapping walls
+   //Last because n^2
+   for(int i = 0;i<map->wall_count;i++)
+   {
+      for(int j = i+1;j<map->wall_count;j++)
+      {
+         if(map->walls[i].portal_wall==j)
+            continue;
+
+         int64_t x0 = map->walls[i].x;
+         int64_t y0 = map->walls[i].y;
+         int64_t x1 = map->walls[map->walls[i].p2].x;
+         int64_t y1 = map->walls[map->walls[i].p2].y;
+         int64_t x2 = map->walls[j].x;
+         int64_t y2 = map->walls[j].y;
+         int64_t x3 = map->walls[map->walls[j].p2].x;
+         int64_t y3 = map->walls[map->walls[j].p2].y;
+
+         int64_t t = ((-(y1-y0)*(x0-x2)+(x1-x0)*(y0-y2))*65536)/RvR_non_zero(-(x3-x2)*(y1-y0)+(x1-x0)*(y3-y2));
+         int64_t u = (((x3-x2)*(y0-y2)-(y3-y2)*(x0-x2))*65536)/RvR_non_zero(-(x3-x2)*(y1-y0)+(x1-x0)*(y3-y2));
+
+         //If walls touch, the value of both will be 0 or 65536, but these are still valid
+         if((t==0||t==65536)&&(u==0||u==65536))
+            continue;
+
+         RvR_error_check((t<0||t>65536)||(u<0||u>65536),"RvR_port_map_check","walls %d and %d intersect (%ld %ld) (%ld,%ld) (%ld,%ld) (%ld,%ld) (%ld,%ld)\n",i,j,t,u,x0,y0,x1,y1,x2,y2,x3,y3);
+      }
    }
 
    return 1;
