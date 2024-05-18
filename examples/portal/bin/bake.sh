04466cefa6f724bf3259f783c4009e41e70164ef
Captain4LK <captain4lk@tutanota.com>
2024-05-18 21:23:01 +0200

    Start portal

---
+++ examples/portal/bin/bake.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo \"pal00000.cop\",\"PAL00000\" > data/main.csv
+echo \"tex61440.rvr\",\"TEX61440\" >> data/main.csv
+echo \"tex61441.rvr\",\"TEX61441\" >> data/main.csv
+
+for f in ../assets/png/*.png; do
+    ../../../tools/texture-tool/texture-tool --in $f --wall --pal ../assets/palette.png --out data/tex`basename $f .png`.rvr;
+
+   echo \"tex`basename $f .png`.rvr\",\"TEX`basename $f .png`\" >> data/main.csv;
+done
---
+++ examples/portal/src/main.c
@@ -0,0 +1,63 @@
+/*
+RvnicRaven - portal
+
+Written in 2024 by Lukas Holzbeierlein (Captain4LK) email: captain4lk [at] tutanota [dot] com
+
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+//External includes
+#include <stdio.h>
+#include <stdint.h>
+#include "RvR/RvR.h"
+#include "RvR/RvR_portal.h"
+//-------------------------------------
+
+//Internal includes
+//-------------------------------------
+
+//#defines
+#define MEM_SIZE (1 << 27)
+//-------------------------------------
+
+//Typedefs
+//-------------------------------------
+
+//Variables
+static uint8_t mem[MEM_SIZE];
+
+static Map *map;
+//-------------------------------------
+
+//Function prototypes
+//-------------------------------------
+
+//Function implementations
+
+int main(int argc, char **argv)
+{
+   RvR_malloc_init(mem, MEM_SIZE);
+
+   RvR_init("RvnicRaven - portal", 0);
+   RvR_key_repeat(1);
+
+   RvR_pak_add("data/main.csv");
+
+   //User defined overwrites (used for modding)
+   for(int i = 1; i<argc; i++)
+      RvR_pak_add(argv[i]);
+
+   RvR_palette_load(0);
+   RvR_render_font_set(0xF000);
+
+   while(RvR_running())
+   {
+      RvR_update();
+      RvR_render_present();
+   }
+
+   return 0;
+}
+//-------------------------------------
